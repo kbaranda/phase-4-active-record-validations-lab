15fe9c2da9b5d4a204a659b24f48857f8cde67f7
Kat <bolivarkat@yahoo.com>
2022-02-19 12:46:12 -0600

    commit tree c212d6abc8393cc937795a33b4734f13d389be3e

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,14 @@
 class Post < ApplicationRecord
+ validates :title, presence: true
+ validates :content, length: {minimum: 250 }
+ validates :summary, length: {maximum: 250 }
+ validates :category, inclusion: ["Fiction", "Non-Fiction"]
+
+ validate :title_is_clickbait
+
+ def title_is_clickbait
+  unless self.title && self.title.match(/Won't Believe|Secret|Top \d+|Guess/)
+   errors.add(:title, "is not clickbaity")
+  end
+ end
 end
